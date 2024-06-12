9e9d040da72959775c830dd488210cd6cf012306
Ricardo Garcia <ricardogarcia.lopez@hotmail.com>
2024-06-12 13:59:45 -0600

    first commit

---
+++ AirbnbTutorial/App/AirbnbTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbTutorialApp.swift
+//  AirbnbTutorial
+//
+//  Created by Ricardo Garcia on 12/06/24.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
